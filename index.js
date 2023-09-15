3162dc9d492c1a0f079b9fa2b9c139af0d582e62
ckorir <chrisruto.k@gmail.com>
2023-09-15 11:16:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Define the function to destructively append a cat to the end of the cats array
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name){
+    const newCats = cats.slice();
+    newCats.splice(newCats.length, 0, name);
+
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = cats.slice();
+    newCats.splice(0, 0, name);
+
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCats = cats.slice();
+    newCats.splice(newCats.length - 1);
+
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = cats.slice();
+    newCats.splice(0, 1);
+
+    return newCats;
+}
